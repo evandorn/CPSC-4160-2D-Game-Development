6fc37f6c5c909e6adc9b34d34e6381190bb181aa
evan.dorn@gmail.com <evan.dorn@gmail.com>
2016-03-04 02:33:54 -0500

    explosion clock

--- asg2/explosions/clock.h
+++ asg2/explosions/clock.h
@@ -1,42 +1,60 @@
+//
+//  clock.h
+//  CPSC-4160-Project-2
+//
+//  Created by Evan Dorn on 2/23/16.
+//  Copyright © 2016 evandorn. All rights reserved.
+//
+
 #include <SDL.h>
 #include <string>
 #include <deque>
-#include "vector2f.h"
 
 class Manager;
 
-// class Clock is a Singleton
 class Clock {
 public:
-  static Clock& getInstance();  
-  unsigned int getTicksSinceLastFrame() const;
-  unsigned int getTicksSinceInit() const { return 0; }
-  unsigned int getSeconds() const { return sumOfTicks/1000;  }
-
-  void toggleSloMo();
-  bool isStarted() const { return started; }
-  bool isPaused() const  { return paused;  }
-  int getFps() const;
-  void toggleCap() { capFrameRate = !capFrameRate; }
-
-  void start();
-  void pause();
-  void unpause();
-  void draw() const;  // For debugging
-  void update();      // Increments ticks
+  static Clock& getInstance();
+  unsigned int getTicks() const;
+  unsigned int getTotalTicks() const { return sumOfAllTicks; }
 
 private:
-  unsigned int frames;
-  unsigned int ticksSinceLastFrame;
-  unsigned int ticksAtLastFrame;
-  unsigned int sumOfTicks;
+  friend class Manager;
 
   bool started;
   bool paused;
   bool sloMo;
-  Vector2f fpsLoc;
-  Vector2f secondsLoc;
-  bool capFrameRate;
+  const bool framesAreCapped;
+  const int frameCap;
+
+  unsigned int frames;
+  std::deque<int> recentFrames;
+  const unsigned int maxFramesToAvg;
+
+  unsigned int tickSum;
+  unsigned int sumOfAllTicks;
+  unsigned int timeAtStart;
+  unsigned int timeAtPause;
+  unsigned int currTicks;
+  unsigned int prevTicks;
+  unsigned int ticks;
+
+  unsigned int getElapsedTicks();
+  Clock& operator++();
+  void toggleSloMo();
+
+  bool isStarted() const { return started; }
+  bool isPaused() const  { return paused;  }
+  unsigned int getFrames() const  { return frames;  }
+  unsigned int getSeconds() const { return getTicks()/1000;  }
+  unsigned int capFrameRate() const;
+  int getFps() const;
+  int getAvgFps() const;
+
+  void start();
+  void pause();
+  void unpause();
+  void display() const;
 
   Clock();
   Clock(const Clock&);
